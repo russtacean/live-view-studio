75440cf8606dded4af51f0341566d93aa73d31af
Russ Gunther <russell.g@leantaas.com>
2024-05-17 20:22:10 -0400

    Chap 12 Exercise: Add sorting support to pizza orders table

--- lib/live_view_studio_web/live/pizza_orders_live.ex
+++ lib/live_view_studio_web/live/pizza_orders_live.ex
@@ -12,4 +12,61 @@ defmodule LiveViewStudioWeb.PizzaOrdersLive do
 
     {:ok, socket}
   end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
+
+  def handle_params(params, uri, socket) do
+    sort_order = valid_sort_order(params)
+    sort_by = valid_sort_by(params)
+
+    options = %{sort_order: sort_order, sort_by: sort_by}
+    pizza_orders = PizzaOrders.list_pizza_orders(options)
+
+    socket = assign(socket, pizza_orders: pizza_orders, options: options)
+    {:noreply, socket}
+  end
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(size style topping_1 topping_2 price) do
+    String.to_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order})
+       when sort_order in ~w(asc desc) do
+    String.to_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
