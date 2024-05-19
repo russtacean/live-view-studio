a353d1388139410290ef7b8366ba2fa446bd48bd
Russ Gunther <russell.g@leantaas.com>
2024-05-19 10:29:14 -0400

    Chap 14: Use form to create entries in the database

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -2,13 +2,17 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Volunteers
+  alias LiveViewStudio.Volunteers.Volunteer
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
       assign(socket,
-        volunteers: volunteers
+        volunteers: volunteers,
+        form: to_form(changeset)
       )
 
     {:ok, socket}
@@ -18,6 +22,14 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
+      <.form for={@form} phx-submit="save">
+        <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
+        <.input field={@form[:phone]} type="tel" placeholder="Phone" />
+        <.button phx-disable-with="Saving...">
+          Check In
+        </.button>
+      </.form>
+
       <div
         :for={volunteer <- @volunteers}
         class={"volunteer #{if volunteer.checked_out, do: "out"}"}
@@ -36,5 +48,24 @@ defmodule LiveViewStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        socket =
+          update(
+            socket,
+            :volunteers,
+            fn volunteers -> [volunteer | volunteers] end
+          )
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+
+        {:noreply, assign(socket, :form, to_form(changeset))}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
   end
 end
