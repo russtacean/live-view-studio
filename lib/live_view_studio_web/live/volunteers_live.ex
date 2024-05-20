fc6ce44ac3594a174c9d04113977b4abeef89ff4
Russ Gunther <russell.g@leantaas.com>
2024-05-20 17:10:18 -0400

    Chap 15: Add debounced live validations for volunteer page

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -22,9 +22,19 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.form for={@form} phx-submit="save">
-        <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
-        <.input field={@form[:phone]} type="tel" placeholder="Phone" />
+      <.form for={@form} phx-change="validate" phx-submit="save">
+        <.input
+          field={@form[:name]}
+          placeholder="Name"
+          autocomplete="off"
+          phx-debounce="1000"
+        />
+        <.input
+          field={@form[:phone]}
+          type="tel"
+          placeholder="Phone"
+          phx-debounce="blur"
+        />
         <.button phx-disable-with="Saving...">
           Check In
         </.button>
@@ -70,5 +80,16 @@ defmodule LiveViewStudioWeb.VolunteersLive do
         socket = put_flash(socket, :error, "Unable to check volunteer in.")
         {:noreply, assign(socket, :form, to_form(changeset))}
     end
+  end
+
+  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
+    changeset =
+      %Volunteer{}
+      |> Volunteers.change_volunteer(volunteer_params)
+      # Action needs to be non-nil for errors to show on form
+      |> Map.put(:action, :validate)
+
+    socket = assign(socket, form: to_form(changeset))
+    {:noreply, socket}
   end
 end
