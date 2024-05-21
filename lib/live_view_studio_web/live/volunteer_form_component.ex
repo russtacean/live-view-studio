3505583f6c925a2fdbe131b77afc8a2a5320f2d5
Russ Gunther <rgunther30@gmail.com>
2024-05-21 17:13:04 -0400

    Chap 18: Create live component for volunteer form, captures both HEEx template and associated event functions

---
+++ lib/live_view_studio_web/live/volunteer_form_component.ex
@@ -0,0 +1,71 @@
+defmodule LiveViewStudioWeb.VolunteerFormComponent do
+  use LiveViewStudioWeb, :live_component
+
+  alias LiveViewStudio.Volunteers
+  alias LiveViewStudio.Volunteers.Volunteer
+
+  def mount(socket) do
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+    {:ok, assign(socket, :form, to_form(changeset))}
+  end
+
+  def render(assigns) do
+    # Live components require a single static HTML component at their root
+    # hence the div here
+    ~H"""
+    <div>
+      <.form
+        for={@form}
+        phx-change="validate"
+        phx-submit="save"
+        phx-target={@myself}
+      >
+        <.input
+          field={@form[:name]}
+          placeholder="Name"
+          autocomplete="off"
+          phx-debounce="1000"
+        />
+        <.input
+          field={@form[:phone]}
+          type="tel"
+          placeholder="Phone"
+          phx-debounce="blur"
+        />
+        <.button phx-disable-with="Saving...">
+          Check In
+        </.button>
+      </.form>
+    </div>
+    """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        # Live components run in the same process as the parent live view using them
+        send(self(), {:volunteer_created, volunteer})
+
+        socket = put_flash(socket, :info, "Volunteer successfully checked in.")
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+
+        {:noreply, assign(socket, :form, to_form(changeset))}
+
+      {:error, changeset} ->
+        socket = put_flash(socket, :error, "Unable to check volunteer in.")
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
+  end
+
+  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
+    changeset =
+      %Volunteer{}
+      |> Volunteers.change_volunteer(volunteer_params)
+      # Action needs to be non-nil for errors to show on form
+      |> Map.put(:action, :validate)
+
+    socket = assign(socket, form: to_form(changeset))
+    {:noreply, socket}
+  end
+end
--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -2,18 +2,11 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Volunteers
-  alias LiveViewStudio.Volunteers.Volunteer
+  alias LiveViewStudioWeb.VolunteerFormComponent
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
-
-    changeset = Volunteers.change_volunteer(%Volunteer{})
-
-    socket =
-      socket
-      |> stream(:volunteers, volunteers)
-      |> assign(:form, to_form(changeset))
-
+    socket = stream(socket, :volunteers, volunteers)
     {:ok, socket}
   end
 
@@ -21,84 +14,47 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.form for={@form} phx-change="validate" phx-submit="save">
-        <.input
-          field={@form[:name]}
-          placeholder="Name"
-          autocomplete="off"
-          phx-debounce="1000"
-        />
-        <.input
-          field={@form[:phone]}
-          type="tel"
-          placeholder="Phone"
-          phx-debounce="blur"
-        />
-        <.button phx-disable-with="Saving...">
-          Check In
-        </.button>
-      </.form>
-
+      <.live_component module={VolunteerFormComponent} id={:new} />
       <div id="volunteers" phx-update="stream">
-        <div
+        <.volunteer
           :for={{volunteer_id, volunteer} <- @streams.volunteers}
-          class={"volunteer #{if volunteer.checked_out, do: "out"}"}
+          volunteer={volunteer}
           id={volunteer_id}
-        >
-          <div class="name">
-            <%= volunteer.name %>
-          </div>
-          <div class="phone">
-            <%= volunteer.phone %>
-          </div>
-          <div class="status">
-            <button phx-click="toggle-status" phx-value-id={volunteer.id}>
-              <%= if volunteer.checked_out,
-                do: "Check In",
-                else: "Check Out" %>
-            </button>
-            <.link
-              class="delete"
-              phx-click="delete"
-              phx-value-id={volunteer.id}
-              data-confirm="Are you sure you want to delete this volunteer?"
-            >
-              <.icon name="hero-trash-solid" />
-            </.link>
-          </div>
-        </div>
+        />
       </div>
     </div>
     """
   end
 
-  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
-    case Volunteers.create_volunteer(volunteer_params) do
-      {:ok, volunteer} ->
-        socket =
-          stream_insert(socket, :volunteers, volunteer, at: 0)
-
-        socket = put_flash(socket, :info, "Volunteer successfully checked in.")
-
-        changeset = Volunteers.change_volunteer(%Volunteer{})
-
-        {:noreply, assign(socket, :form, to_form(changeset))}
-
-      {:error, changeset} ->
-        socket = put_flash(socket, :error, "Unable to check volunteer in.")
-        {:noreply, assign(socket, :form, to_form(changeset))}
-    end
-  end
-
-  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
-    changeset =
-      %Volunteer{}
-      |> Volunteers.change_volunteer(volunteer_params)
-      # Action needs to be non-nil for errors to show on form
-      |> Map.put(:action, :validate)
-
-    socket = assign(socket, form: to_form(changeset))
-    {:noreply, socket}
+  def volunteer(assigns) do
+    ~H"""
+    <div
+      class={"volunteer #{if @volunteer.checked_out, do: "out"}"}
+      id={@id}
+    >
+      <div class="name">
+        <%= @volunteer.name %>
+      </div>
+      <div class="phone">
+        <%= @volunteer.phone %>
+      </div>
+      <div class="status">
+        <button phx-click="toggle-status" phx-value-id={@volunteer.id}>
+          <%= if @volunteer.checked_out,
+            do: "Check In",
+            else: "Check Out" %>
+        </button>
+        <.link
+          class="delete"
+          phx-click="delete"
+          phx-value-id={@volunteer.id}
+          data-confirm="Are you sure you want to delete this volunteer?"
+        >
+          <.icon name="hero-trash-solid" />
+        </.link>
+      </div>
+    </div>
+    """
   end
 
   def handle_event("toggle-status", %{"id" => id}, socket) do
@@ -113,5 +69,9 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     volunteer = Volunteers.get_volunteer!(id)
     {:ok, _} = Volunteers.delete_volunteer(volunteer)
     {:noreply, stream_delete(socket, :volunteers, volunteer)}
+  end
+
+  def handle_info({:volunteer_created, volunteer}, socket) do
+    {:noreply, stream_insert(socket, :volunteers, volunteer, at: 0)}
   end
 end
