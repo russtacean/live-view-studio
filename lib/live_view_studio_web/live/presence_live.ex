3fe0b9f023211d320b502a9496918b99ad81362d
Russ Gunther <rgunther30@gmail.com>
2024-05-29 20:36:26 -0400

    Chap 23 Exercise: Toggle visibility of presnece list with a styled button

--- lib/live_view_studio_web/live/presence_live.ex
+++ lib/live_view_studio_web/live/presence_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.PresenceLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudioWeb.Presence
+  alias Phoenix.LiveView.JS
 
   @topic "users:video"
 
@@ -26,8 +27,13 @@ defmodule LiveViewStudioWeb.PresenceLive do
     ~H"""
     <div id="presence">
       <div class="users">
-        <h2>Who's Here?</h2>
-        <ul>
+        <h2>
+          Who's Here?
+          <button phx-click={toggle_presences()}>
+            <.icon name="hero-list-bullet-solid" />
+          </button>
+        </h2>
+        <ul id="presences">
           <li :for={{_user_id, meta} <- @presences}>
             <span class="status">
               <%= if meta.is_playing, do: "👀", else: "🙈" %>
@@ -65,5 +71,17 @@ defmodule LiveViewStudioWeb.PresenceLive do
 
   def handle_info(%{event: "presence_diff", payload: diff}, socket) do
     {:noreply, Presence.handle_diff(socket, diff)}
+  end
+
+  def toggle_presences do
+    JS.toggle(to: "#presences")
+    |> JS.remove_class(
+      "bg-slate-400",
+      to: ".hero-list-bullet-solid.bg-slate-400"
+    )
+    |> JS.add_class(
+      "bg-slate-400",
+      to: ".hero-list-bullet-solid:not(.bg-slate-400)"
+    )
   end
 end
